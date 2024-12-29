d1284e1ad10eac79652a8057c321c3facd0fd28a
Maks Balamut <balamutmaks@gmail.com>
2024-12-29 23:51:45 +0200

    Refactoring ServerHttpResponseDecorator

--- services/web-bff/build.gradle.kts
+++ services/web-bff/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 val springCloudVersion by extra("2024.0.0")
 
 group = "com.balamut"
-version = "0.1.21-TEST"
+version = "0.1.23-TEST"
 
 java {
     toolchain {
--- services/web-bff/src/main/java/com/balamut/webbff/authentication/RetryAuthenticationFilter.java
+++ services/web-bff/src/main/java/com/balamut/webbff/authentication/RetryAuthenticationFilter.java
@@ -1,11 +1,11 @@
 package com.balamut.webbff.authentication;
 
+import com.balamut.webbff.http.EmptyHeadersHttpResponseDecorator;
 import lombok.RequiredArgsConstructor;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
 import org.springframework.cloud.gateway.support.ServerWebExchangeUtils;
 import org.springframework.core.Ordered;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.http.server.reactive.ServerHttpResponseDecorator;
@@ -13,13 +13,6 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
 import reactor.netty.Connection;
-import reactor.retry.Repeat;
-
-import java.util.Collections;
-import java.util.Set;
-
-import static org.springframework.cloud.gateway.support.ServerWebExchangeUtils.CLIENT_RESPONSE_HEADER_NAMES;
-import static org.springframework.cloud.gateway.support.ServerWebExchangeUtils.GATEWAY_ALREADY_ROUTED_ATTR;
 
 @Component
 @RequiredArgsConstructor
@@ -31,37 +24,33 @@ public class RetryAuthenticationFilter implements GlobalFilter, Ordered {
                 .then(Mono.defer(() -> {
                     if (exchange.getResponse().getStatusCode() == HttpStatus.FORBIDDEN) {
                         ServerWebExchange mutatedExchange = mutateRequest(exchange, "new-token");
-                        reset(exchange);
-                        return chain.filter(mutatedExchange);
+                        reset(mutatedExchange);
+                        return chain.filter(reset(mutatedExchange));
                     }
                     return Mono.empty();
                 }));
     }
 
-    private void reset(ServerWebExchange exchange) {
+    private ServerWebExchange reset(ServerWebExchange exchange) {
         Connection conn = exchange.getAttribute(ServerWebExchangeUtils.CLIENT_RESPONSE_CONN_ATTR);
         if (conn != null) {
             conn.dispose();
             exchange.getAttributes().remove(ServerWebExchangeUtils.CLIENT_RESPONSE_CONN_ATTR);
         }
         ServerWebExchangeUtils.removeAlreadyRouted(exchange);
-        exchange.getResponse().getHeaders().clearContentHeaders();
+        ServerHttpResponseDecorator response = new EmptyHeadersHttpResponseDecorator(exchange);
+        return exchange.mutate()
+                .response(response)
+                .build();
     }
 
     private ServerWebExchange mutateRequest(ServerWebExchange exchange, String token) {
         ServerHttpRequest request = exchange.getRequest().mutate()
                 .headers(headers -> headers.setBearerAuth(token))
                 .build();
-        ServerHttpResponseDecorator response = new ServerHttpResponseDecorator(exchange.getResponse()) {
-            @Override
-            public HttpHeaders getHeaders() {
-                HttpHeaders headers = HttpHeaders.writableHttpHeaders(super.getHeaders());
-                Set<String> addedHeaders = exchange.getAttributeOrDefault(CLIENT_RESPONSE_HEADER_NAMES, Collections.emptySet());
-                addedHeaders.forEach(headers::remove);
-                return headers;
-            }
-        };
-        return exchange.mutate().request(request).response(response).build();
+        return exchange.mutate()
+                .request(request)
+                .build();
     }
 
     @Override
