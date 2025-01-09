f14675fcbf0fd802731354f519fed415fbee1a05
Maks Balamut <balamutmaks@gmail.com>
2025-01-09 17:54:33 +0200

    Implemented SubjectService as HttpClientSubjectService

---
+++ application/src/app/services/subject/http-client-subject.service.ts
@@ -0,0 +1,57 @@
+import {Pupil} from "./pupil";
+import {Subject} from "./subject";
+import {SubjectService} from "./subject.service";
+import {HttpClient} from "@angular/common/http";
+import {Injectable} from "@angular/core";
+import {HttpClientHelper} from "../../clients/http-client-helper";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HttpClientSubjectService implements SubjectService {
+
+  constructor(private http: HttpClient) {
+  }
+
+  getAllSubjects(): Promise<Subject[]> {
+    return new Promise<Subject[]>(((resolve, reject) => {
+      this.http.get<Subject[]>(HttpClientHelper.buildUrl("/api/v1/subjects"))
+        .subscribe({next: value => resolve(value), error: error => reject(error)});
+    }));
+  }
+
+  createSubject(subjectForm: { name: string; description: string; teacher_id: number; }): Promise<Subject> {
+    return new Promise<Subject>(((resolve, reject) => {
+      this.http.post<Subject>(HttpClientHelper.buildUrl("/api/v1/subjects"), subjectForm)
+        .subscribe({next: value => resolve(value), error: error => reject(error)});
+    }));
+  }
+
+  deleteSubject(id: number): Promise<boolean> {
+    return new Promise<boolean>(((resolve, reject) => {
+      this.http.delete(HttpClientHelper.buildUrl(`/api/v1/subjects/${id}`))
+        .subscribe({next: () => resolve(true), error: error => reject(error)});
+    }));
+  }
+
+  getPupilsBySubject(id: number): Promise<Pupil[]> {
+    return new Promise<Pupil[]>(((resolve, reject) => {
+      this.http.get<Pupil[]>(HttpClientHelper.buildUrl(`/api/v1/subjects/${id}/pupils`))
+        .subscribe({next: value => resolve(value), error: error => reject(error)});
+    }));
+  }
+
+  addPupilsToSubject(subjectId: number, pupilIds: number[]): Promise<boolean> {
+    return new Promise<boolean>(((resolve, reject) => {
+      this.http.put<boolean>(HttpClientHelper.buildUrl(`/api/v1/subjects/${subjectId}/pupils`), pupilIds)
+        .subscribe({next: () => resolve(true), error: error => reject(error)});
+    }));
+  }
+
+  deletePupilsFromSubject(id: number, pupilIds: number[]): Promise<boolean> {
+    return new Promise<boolean>(((resolve, reject) => {
+      this.http.delete<boolean>(HttpClientHelper.buildUrl(`/api/v1/subjects/${id}/pupils`), {body: pupilIds})
+        .subscribe({next: () => resolve(true), error: error => reject(error)});
+    }));
+  }
+}
--- application/src/app/services/subject/subject.service.ts
+++ application/src/app/services/subject/subject.service.ts
@@ -1,3 +1,11 @@
-export interface SubjectService {
+import {Subject} from "./subject";
+import {Pupil} from "./pupil";
 
+export interface SubjectService {
+  getAllSubjects: () => Promise<Subject[]>;
+  createSubject: (subjectForm: {name: string; description: string; teacher_id: number}) => Promise<Subject>;
+  deleteSubject: (id: number) => Promise<boolean>;
+  getPupilsBySubject: (id: number) => Promise<Pupil[]>;
+  addPupilsToSubject: (subjectId: number, pupilIds: number[]) => Promise<boolean>;
+  deletePupilsFromSubject: (id: number, pupilIds: number[]) => Promise<boolean>;
 }
