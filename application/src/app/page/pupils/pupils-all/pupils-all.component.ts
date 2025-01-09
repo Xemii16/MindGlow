0042c3c1c32036c0426b10339c2eff8fab29cc74
Maks Balamut <balamutmaks@gmail.com>
2025-01-10 00:47:09 +0200

    Integrated UserService with pupils-all.component

--- application/src/app/page/pupils/pupils-all/pupils-all.component.ts
+++ application/src/app/page/pupils/pupils-all/pupils-all.component.ts
@@ -2,15 +2,15 @@ import {Component, OnInit} from '@angular/core';
 import {MatDivider} from "@angular/material/divider";
 import {MatCheckbox} from "@angular/material/checkbox";
 import {MatButton, MatIconButton} from "@angular/material/button";
-import {MatFormField, MatLabel, MatPrefix} from "@angular/material/form-field";
-import {MatInput} from "@angular/material/input";
 import {MatIcon} from "@angular/material/icon";
-import {FormControl, FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 import {MatList, MatListItem, MatListItemMeta, MatListItemTitle} from "@angular/material/list";
 import {NgForOf, NgIf} from "@angular/common";
 import {MatMenu, MatMenuItem, MatMenuTrigger} from "@angular/material/menu";
 import {MatDialog} from "@angular/material/dialog";
 import {PupilDeleteConfirmComponent} from "./pupil-delete-confirm/pupil-delete-confirm.component";
+import {HttpClientUserService} from "../../../services/user/http-client-user.service";
+import {User} from "../../../services/user/user";
 
 @Component({
   selector: 'app-pupils-all',
@@ -19,12 +19,8 @@ import {PupilDeleteConfirmComponent} from "./pupil-delete-confirm/pupil-delete-c
     MatDivider,
     MatCheckbox,
     MatButton,
-    MatFormField,
     MatIcon,
-    MatLabel,
     MatListItemMeta,
-    MatPrefix,
-    MatInput,
     ReactiveFormsModule,
     FormsModule,
     MatList,
@@ -41,62 +37,43 @@ import {PupilDeleteConfirmComponent} from "./pupil-delete-confirm/pupil-delete-c
   styleUrl: './pupils-all.component.scss'
 })
 export class PupilsAllComponent implements OnInit {
-  hasNext: boolean = false;
-  currentPage: number = 0;
-  startWith: FormControl = new FormControl('');
+  user?: User;
+  pupils: User[] = [];
 
   constructor(
-    public dialog: MatDialog
+    public dialog: MatDialog,
+    private userService: HttpClientUserService
   ) {
   }
 
   ngOnInit(): void {
     this.getPupils();
-    /*this.userService.getUserByToken().then(user => {
-      if (user == null) return;
+    this.userService.getCurrentUser().then(user => {
       this.user = user;
-    });*/
+    })
   }
 
   reloadPages() {
-    this.currentPage = 0;
     /*this.pupils = [];*/
     this.getPupils();
   }
 
-  loadMore() {
-    if (!this.hasNext) return;
-    this.getPupils();
-  }
-
-  openDialog(id: string) {
-    const dialogRef = this.dialog.open(PupilDeleteConfirmComponent, {
-      data: {
-        user: {
-          id: id
+  openDialog(id: number) {
+    this.userService.getUserById(id).then(user => {
+      const dialogRef = this.dialog.open(PupilDeleteConfirmComponent, {
+        data: {
+          user: user
         }
-      }
-    });
-    dialogRef.afterClosed().subscribe(() => {
-      this.reloadPages();
-    });
+      });
+      dialogRef.afterClosed().subscribe(() => {
+        this.reloadPages();
+      });
+    })
   }
 
   private getPupils() {
-    if (this.startWith.value === '') {
-      /*this.userService.getPupilsWithPagination(true, this.currentPage).then((response) => {
-        this.currentPage++;
-        this.hasNext = response?.hasNext ?? false;
-        if (response === null) return;
-        this.pupils.push(...response.users);
-      });*/
-    } else {
-      /*this.userService.getPupilsWithPaginationAndStartWith(true, this.startWith.value, this.currentPage).then((response) => {
-        this.currentPage++;
-        this.hasNext = response?.hasNext ?? false;
-        if (response === null) return;
-        this.pupils.push(...response.users);
-      });*/
-    }
+    this.userService.getAllUsers('pupils').then(users => {
+      this.pupils = users;
+    });
   }
 }
