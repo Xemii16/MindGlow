dde44e9c2e411c2e933d2fd6dd4afd75a17b5833
Maks Balamut <balamutmaks@gmail.com>
2025-01-10 00:19:26 +0200

    Deleted pagination and search in requests.component

--- application/src/app/page/requests/requests.component.ts
+++ application/src/app/page/requests/requests.component.ts
@@ -1,14 +1,12 @@
 import {Component, OnInit} from '@angular/core';
-import {FormControl, FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 import {HttpClientUserService} from "../../services/user/http-client-user.service";
 import {User} from "../../services/user/user";
 import {MatIcon} from "@angular/material/icon";
 import {MatActionList, MatListItem, MatListItemLine, MatListItemTitle} from "@angular/material/list";
-import {MatFormField} from "@angular/material/form-field";
 import {MatDivider} from "@angular/material/divider";
 import {MatCheckbox} from "@angular/material/checkbox";
 import {MatButton} from "@angular/material/button";
-import {MatInput} from "@angular/material/input";
 import {RouterLink} from "@angular/router";
 import {NgForOf, NgIf} from "@angular/common";
 
@@ -22,11 +20,9 @@ import {NgForOf, NgIf} from "@angular/common";
     MatListItemLine,
     MatListItemTitle,
     MatActionList,
-    MatFormField,
     MatDivider,
     MatCheckbox,
     MatButton,
-    MatInput,
     RouterLink,
     MatListItem,
     NgIf,
@@ -38,27 +34,14 @@ import {NgForOf, NgIf} from "@angular/common";
 export class RequestsComponent implements OnInit {
 
   hasNext: boolean = true;
-  currentPage: number = 0;
-  startWith: FormControl = new FormControl('');
   users: User[] = [];
 
   constructor(
     private userService: HttpClientUserService,
   ) {
-
   }
 
   ngOnInit(): void {
-    this.getUsers();
-  }
-
-  reloadPages() {
-    this.currentPage = 0;
-    this.getUsers();
-  }
-
-  loadMore() {
-    if (!this.hasNext) return;
     this.getUsers();
   }
 
