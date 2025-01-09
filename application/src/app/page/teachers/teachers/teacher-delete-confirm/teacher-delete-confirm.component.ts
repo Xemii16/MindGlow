a38cb24886eb80751252d9baaf1c6edb2a476f8e
Color <ilyakoltsovsky@gmail.com>
2025-01-09 23:50:15 +0200

    Updated teacher-delete-confirm.component.ts

--- application/src/app/page/teachers/teachers/teacher-delete-confirm/teacher-delete-confirm.component.ts
+++ application/src/app/page/teachers/teachers/teacher-delete-confirm/teacher-delete-confirm.component.ts
@@ -10,6 +10,8 @@ import {MatFormFieldModule} from "@angular/material/form-field";
 import {MatInputModule} from "@angular/material/input";
 import {MatButtonModule} from "@angular/material/button";
 import {FormsModule} from "@angular/forms";
+import {HttpClientUserService} from "../../../../services/user/http-client-user.service";
+import {User} from "../../../../services/user/user";
 
 @Component({
   selector: 'teacher-delete-confirmation',
@@ -28,14 +30,19 @@ import {FormsModule} from "@angular/forms";
   styleUrl: './teacher-confirm.component.scss'
 })
 export class TeacherDeleteConfirmComponent implements OnInit {
+  user ?: User;
 
   constructor(
     public dialogRef: MatDialogRef<TeacherDeleteConfirmComponent>,
     @Inject(MAT_DIALOG_DATA) public data: RequestDeleteConfirmData,
+    private userService: HttpClientUserService,
   ) {
   }
 
   ngOnInit(): void {
+    this.userService.getUserById(this.data.user.id).then(user => {
+      this.user = user;
+    })
   }
 
   onNoClick(): void {
@@ -43,8 +50,14 @@ export class TeacherDeleteConfirmComponent implements OnInit {
   }
 
   deleteUser() {
+    this.userService.deleteUserById(this.data.user.id).then(Answer => {
+      if (Answer) {
+        this.dialogRef.close();
+      }
+    })
   }
 }
 
 export interface RequestDeleteConfirmData {
+  user: User;
 }
