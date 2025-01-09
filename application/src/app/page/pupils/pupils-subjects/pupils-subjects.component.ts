93271734eaf72df479ca3071338c290b634c2ef5
Color <ilyakoltsovsky@gmail.com>
2025-01-09 22:36:47 +0200

    Updated pupils-subjects.component.ts, BUT WITH ISSUE

--- application/src/app/page/pupils/pupils-subjects/pupils-subjects.component.ts
+++ application/src/app/page/pupils/pupils-subjects/pupils-subjects.component.ts
@@ -1,77 +1,57 @@
-import { Component } from '@angular/core';
-import {MatButton, MatIconButton} from "@angular/material/button";
-import {MatCheckbox} from "@angular/material/checkbox";
-import {MatDivider} from "@angular/material/divider";
-import {MatFormField, MatLabel, MatPrefix} from "@angular/material/form-field";
-import {MatIcon} from "@angular/material/icon";
-import {MatInput} from "@angular/material/input";
-import {MatList, MatListItem, MatListItemMeta, MatListItemTitle} from "@angular/material/list";
-import {MatMenu, MatMenuItem} from "@angular/material/menu";
-import {NgForOf, NgIf} from "@angular/common";
-import {FormControl, ReactiveFormsModule} from "@angular/forms";
-import {MatDialog} from "@angular/material/dialog";
-import {PupilDeleteConfirmComponent} from "../pupils-all/pupil-delete-confirm/pupil-delete-confirm.component";
+import {Component} from '@angular/core';
+import {ReactiveFormsModule} from "@angular/forms";
+import {HttpClientUserService} from "../../../services/user/http-client-user.service";
+import {SubjectService} from "../../../services/subject/subject.service";
+import {User} from "../../../services/user/user";
+import {Subject} from "rxjs";
+import {Pupil} from "../../../services/subject/pupil";
 
 @Component({
   selector: 'app-pupils-subjects',
   standalone: true,
-    imports: [
-        MatButton,
-        MatCheckbox,
-        MatDivider,
-        MatFormField,
-        MatIcon,
-        MatIconButton,
-        MatInput,
-        MatLabel,
-        MatList,
-        MatListItem,
-        MatListItemMeta,
-        MatListItemTitle,
-        MatMenu,
-        MatMenuItem,
-        MatPrefix,
-        NgForOf,
-        NgIf,
-        ReactiveFormsModule
-    ],
+  imports: [
+    ReactiveFormsModule
+  ],
   templateUrl: './pupils-subjects.component.html',
   styleUrl: './pupils-subjects.component.scss'
 })
 export class PupilsSubjectsComponent {
   subjectsStudents: SubjectStudents[] = [];
+  user: User | null = null;
 
   constructor(
+    private userService: HttpClientUserService,
+    private subjectService: SubjectService,
   ) {
   }
 
   ngOnInit(): void {
     this.getStudents();
-    /*this.userService.getUserByToken().then(user => {
-      if (user == null) return;
+    this.userService.getCurrentUser().then((user) => {
+      if (user === null) return;
       this.user = user;
-    });*/
+    })
   }
 
   private getStudents() {
-    /*this.subjectService.getSubjects().then(subjects => {
+    this.subjectService.getAllSubjects().then(subjects => {
       if (subjects == null) {
         return;
       }
-      this.subjects.push(...subjects);
-      this.subjects.forEach(subject => {
-        this.subjectService.getStudents(subject.id).then(students => {
+      subjects.push(...subjects);
+      subjects.forEach(subject => {
+        this.subjectService.getPupilsBySubject(subject.id).then(students => {
           if (students == null) {
             return;
           }
           this.subjectsStudents.push({subject, students});
-        });
+        })
       });
-    });*/
+    });
   }
 }
 
-interface SubjectStudents {
-/*  subject: SubjectResponse;
-  students: UserResponse[];*/
+  interface SubjectStudents {
+    // subject: Subject;
+    // students: Pupil[];
 }
